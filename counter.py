de37f0333fb824e20c2a94c6cd65d1da7070ac82
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-28 13:03:07 +0100

    feat(countr):

--- counter.py
+++ counter.py
@@ -1,10 +1,17 @@
 class count:
-    def ages(self):
-        self.current_year = 2017
-        self.birth_year = 1986
-        self.age = self.current_year - self.birth_year
-        print("My age is: " + str(self.age))
-        if self.age > 50:
-            print("I am too old for festivals")
+
+  def ages(self, name):
+    self.name = name
+    self.current_year = 2017
+    self.birth_year = 1986
+    self.age = self.current_year - self.birth_year
+    print("My age is: " + str(self.age))
+    print(self.name)
+    if self.age > 50:
+      print("I am too old for festivals")
+        
 count_age = count()
-count_age.ages()
+count_age.ages("Tamara")
+
+
+
