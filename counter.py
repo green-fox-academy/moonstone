74928f045015a2098806c1726df774bd26d6cb58
peter-keller <keller.f.peter@gmail.com>
2017-11-28 14:05:50 +0100

    feat(counter): c6

--- counter.py
+++ counter.py
@@ -1,17 +1,19 @@
 class count:
 
-  def ages(self, name):
+  def ages(self, name, number):
+    self.number = number
     self.name = name
     self.current_year = 2017
     self.birth_year = 1986
     self.month = 8
     self.age = self.current_year - self.birth_year
+    print("Counting the countless: " + str(self.number))
     print("I borned in the: " + str(self.month)+ "th month") 
     if self.age > 50:
       print("I am too old for festivals")
         
 count_age = count()
-count_age.ages("Tamara")
+count_age.ages("Tamara", 17)
 
 
 
