52c2e629603d7cbed20e6374f0b2ffd23ace10cf
peter-keller <keller.f.peter@gmail.com>
2017-11-28 13:07:31 +0100

    feat(cheer): c2

--- cheer.py
+++ cheer.py
@@ -1,6 +1,8 @@
 class cheer:
-    def cheer_method(self):
+    def cheer_method(self, member_count):
+        self.member_count = member_count
+        print(self.member_count)
         print("Moonstone")
 
 teamname = cheer()
-teamname.cheer_method():
+teamname.cheer_method(5)
