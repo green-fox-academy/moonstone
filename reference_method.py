3685f447f5ad10950c75f160649d50fc770d9edd
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-28 11:51:15 +0100

    feat(count function): B create a count function

--- reference_method.py
+++ reference_method.py
@@ -4,3 +4,13 @@ class greet:
 
 sample = greet()
 sample.name() 
+
+class count:
+    def ages(self):
+        self.current_year = 2017
+        self.birth_year = 1986
+        self.age = self.current_year - self.birth_year
+        print("My age is: " + str(self.age))
+
+count_age = count()
+count_age.ages()
