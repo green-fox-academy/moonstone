0ba91154d565cae9f1f88eb5bde71ff9eb3ea203
Fülöp Zsuzsi <flp.zsuzsi@gmail.com>
2017-11-28 14:08:19 +0100

    feat(cheer.py): a6

--- cheer.py
+++ cheer.py
@@ -1,9 +1,14 @@
 class cheer:
-    def cheer_method(self, member_count):
+    def cheer_method(self, member_count, parameter):
         self.member_count = member_count
         print(self.member_count)
         print("Moonstone")
         print("hello")
+        print("Cheer for " + parameter)
+    
+    def future(self, params):
+        print("your future looks like " + params)
 
 teamname = cheer()
-teamname.cheer_method(5)
+teamname.cheer_method(5, "R2D2")
+teamname.future("happy")
