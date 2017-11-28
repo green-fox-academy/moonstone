9e63520f791dcd8806ef29f84cc728a361cbad01
peter-keller <keller.f.peter@gmail.com>
2017-11-28 16:31:29 +0100

    feat(reference): fixed

--- reference_method.py
+++ reference_method.py
@@ -1,9 +1,11 @@
 class greet:
     def name(self, name, age, school):
         self.school = school
-        print("Greetings from " + name + " i am " + age + " years old.")
+        self.name = name
+        self.age = age
+        print("Greetings from " + self.name + " i am " + self.age + " years old.")
         print("Greetings for the " + self.school)
 
 
 sample = greet()
-sample.name("Zsuzsi", 25, "Green Fox") 
+sample.name("Zsuzsi", "25", "Green Fox") 
